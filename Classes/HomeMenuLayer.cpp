ac454d6d87ff15b4ec60ebfa1cc9a011cb2b1ba2
Louzhenghao <louzhenghao@louzhdeMacBook-Pro.local>
2015-04-13 13:53:37 +0800

    louzh home selected scene

---
+++ Classes/HomeMenuLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  HomeMenuLayer.cpp
+//  LostRoutes
+//
+//  Created by Louzhenghao on 15/4/13.
+//
+//
+
+#include "HomeMenuLayer.h"
---
+++ Classes/HomeMenuLayer.h
@@ -0,0 +1,14 @@
+//
+//  HomeMenuLayer.h
+//  LostRoutes
+//
+//  Created by Louzhenghao on 15/4/13.
+//
+//
+
+#ifndef __LostRoutes__HomeMenuLayer__
+#define __LostRoutes__HomeMenuLayer__
+
+#include <stdio.h>
+
+#endif /* defined(__LostRoutes__HomeMenuLayer__) */
